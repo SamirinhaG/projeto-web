cd4708f11c773d68d80b7fe77628e17890d64242
Samira Gomes <samiragomes@alu.ufc.br>
2026-06-29 23:00:46 -0300

    Delete arquivos front directory

--- arquivos front/js/home.js
+++
@@ -1,5 +0,0 @@
-// O botão "Entrar" na sua home precisa de um ouvinte
-document.querySelector('.btn-secondary').addEventListener('click', (e) => {
-    // Se o seu login estiver em outra página:
-    window.location.href = 'login.html'; 
-});
